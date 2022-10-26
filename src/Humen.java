b4b509cf847d8dcb63c588f7eea4b3134ae0f357
Лидия Дворкина <lideya832@mail.ru>
2022-10-26 21:46:24 +0300

    Домашнее задание: курс 2, задача с людьми

--- src/Humen.java
+++ src/Humen.java
@@ -1,19 +1,23 @@
 import java.time.LocalDate;
 
 public class Humen {
-    String name;
-    String town;
-    int yearOfBirth;
+    public String name;
+    public String town;
+    public int yearOfBirth;
+    public String job;
 
-    String job;
-
-
-
-    Humen (String name) {
-        this.name = name;
+    public Humen () {
+        this.name = "информация не указана";
+        this.town = "информация не указана";
+        if (yearOfBirth>=0){
+            this.yearOfBirth = yearOfBirth;
+        }else {
+            this.yearOfBirth = 0;
+        }
+        this.job = "информация не указана";
 
     }
-    void man() {
+    void man () {
         System.out.println("Привет! Меня зовут " + name + ". Я из города " + town + ". Я родился в "
                 + yearOfBirth + " году. Я работаю на должности " + job + ". Будем знакомы!");
     }
--- src/Main.java
+++ src/Main.java
@@ -3,30 +3,31 @@ import java.time.LocalDate;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Задание № 1");
-        System.out.println();
-    Humen maxim = new Humen ("Максим");
-        maxim.town = "Минск";
-        maxim.yearOfBirth= LocalDate.now().getYear() - 35;
-        maxim.job = "бренд-менеджера";
+       Humen maxim = new Humen ();
+
+       maxim.town = "Минск";
+       maxim.yearOfBirth = 1987;
+       maxim.job = "бренд-менеджера";
         maxim.man ();
 
-        Humen any = new Humen ("Аня");
-        any.town = "Москва";
-        any.yearOfBirth = LocalDate.now().getYear() - 29;
+        Humen any = new Humen ();
+        any.name = "Аня";
+
+        any.yearOfBirth = 1993;
         any.job = "методиста образовательных программ";
         any.lady ();
 
-        Humen katy = new Humen ("Катя");
+        Humen katy = new Humen ();
+        katy.name = "Катя";
         katy.town = "Калининград";
-        katy.yearOfBirth = LocalDate.now().getYear() - 28;
+
         katy.job = "продакт-менеджера";
         katy.lady ();
 
-        Humen artem = new Humen ("Артем");
+        Humen artem = new Humen ();
+        artem.name = "Артем";
         artem.town = "Москва";
-        artem.yearOfBirth = LocalDate.now().getYear() - 27;
-        artem.job = "директором по развитию бизнеса";
+        artem.yearOfBirth = 1995;
         artem.man ();
 
 
